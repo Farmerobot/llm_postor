4162a9c27696c96001516e8a61bbca8a6321d8c8
Luncenok <mateusz.idziejczak@gmail.com>
2024-10-22 00:43:33 +0200

    sidebar container

--- src/game/gui_handler.py
+++ src/game/gui_handler.py
@@ -14,28 +14,46 @@ class GUIHandler(BaseModel):
     game_log_json: Optional[DeltaGenerator] = None
     cols: List[DeltaGenerator] = Field(default_factory=list)
     model_config = {"arbitrary_types_allowed": True}
-
-    def update_gui(self, game_state: GameState):
+    sidebar: List[DeltaGenerator] = Field(default_factory=list)
+    
+    def init_gui(self, game_state: GameState):
         num_players = len(game_state.players)
-
-        # Create columns and placeholders only on the first call
         if not self.cols:
             self.cols = [col.empty() for col in st.columns(num_players)]
             for col in self.cols:
                 self.player_states_placeholders.append(col.empty())
             # self.player_states_placeholders = [st.empty() for _ in range(num_players)]
+        if not self.sidebar:
+            sidebar = st.sidebar
+            with sidebar:
+                for _ in game_state.players:
+                    self.sidebar.append(st.empty())
         if not self.game_log_placeholder:
             self.game_log_placeholder = st.empty()
         if not self.game_log_json:
             self.game_log_json = st.empty()
+        
 
-        # Update existing widgets
+    def update_gui(self, game_state: GameState):
+        self.init_gui(game_state)
         for i, (player, col) in enumerate(zip(game_state.players, self.cols)):
             with col:
                 self._display_player_info(player, self.player_states_placeholders[i])
+                
+        for i, (player, sidebar) in enumerate(zip(game_state.players, self.sidebar)):
+            with sidebar:
+                self._display_short_player_info(player, st)
         
         self.game_log_placeholder.text("\n".join(game_state.playthrough))
         self.game_log_json.json(game_state.to_dict(), expanded=True)
+        
+    def _display_short_player_info(self, player: Player, placeholder: DeltaGenerator):
+        with placeholder.container(border=True): 
+            st.subheader(player.name)
+            self._display_status(player)
+            self._display_role(player)
+            self._display_tasks(player)
+        
 
     def _display_player_info(self, player: Player, placeholder: DeltaGenerator):
         with placeholder.container():  # Clear previous content
