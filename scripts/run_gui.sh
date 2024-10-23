89b0bcafb086ba9fc97c151e9a2636989561d879
Mateusz Stawicki <mateusz.stawicki@3lance.pl>
2024-10-23 10:28:13 +0200

    fix broken script

--- scripts/run_gui.sh
+++ scripts/run_gui.sh
@@ -6,9 +6,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Navigate to the project root (assuming this script is located inside `scripts/` directory)
 cd "$SCRIPT_DIR/.."
 
-# Activate the poetry environment
-poetry shell
-
-# Run the streamlit app from the `src` directory
-cd src
-streamlit run demo_gui.py
+# Run the Streamlit app in the Poetry environment
+poetry run streamlit run src/demo_gui.py
