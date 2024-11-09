bfefcc6fceca41683212dabd7074435840d79c91
Vasyl Korzavatykh <vasia@korzavatykh.com>
2024-11-09 18:42:11 +0100

    tournament update

--- src/llm_postor/tournament.py
+++ src/llm_postor/tournament.py
@@ -1,33 +1,136 @@
 from llm_postor.game.game_engine import GameEngine
 from llm_postor.game.players.ai import AIPlayer
-from llm_postor.game.chat_analyzer import ChatAnalyzer
+from llm_postor.game.models.engine import GamePhase
+import os
 from random import shuffle
+from itertools import permutations
 
-crewmates_model = "google/gemini-flash-1.5"
-impostor_model = "openai/gpt-4o-mini"
 
-players = [
-    AIPlayer(name="Mateusz", llm_model_name=impostor_model, role="Impostor"),
-    AIPlayer(name="Wojtek", llm_model_name=crewmates_model),
-    AIPlayer(name="Lolek", llm_model_name=crewmates_model),
-    AIPlayer(name="Norbert", llm_model_name=crewmates_model),
-    AIPlayer(name="Nemo", llm_model_name=crewmates_model),
+# Variables
+n_repetitions=1
+n_players = 5
+n_impostors = 1
+dir = 'data/tournament'
+n_round_cut_off = 3
+debug = True
+ai_models = [
+    "openai/gpt-4o-mini",
+    "google/gemini-flash-1.5",
+    "meta-llama/llama-3.1-8b-instruct"
 ]
-shuffle(players)
 
-game_engine = GameEngine()
-game_engine.init_game()
-game_engine.state.DEBUG = True
-game_engine.load_players(players, impostor_count=1)
 
-res = False
-while not res:
-    try:
-        res = game_engine.perform_step()
-    except Exception as e:
-        if "LLM did" in str(e):
-            continue
-        else:
-            raise e
+class TournamentGame():
+    def __init__(self, n_players, n_impostors, impostor_model, crewmate_model, dir, file_path, n_round_cut_off, debug=False):
+        self.n_players = n_players
+        self.n_impostors = n_impostors
+        self.impostor_model = impostor_model
+        self.crewmate_model = crewmate_model
+        self.dir = dir
+        self.file_path = file_path
+        self.n_round_cut_off = n_round_cut_off
+        self.debug = debug
+        self.full_path = os.path.join(dir, file_path)
 
-if game_engine.state.DEBUG: print("Total cost:", game_engine.state.get_total_cost())
+        self.player_names = [
+            "Alice", "Bob", "Charlie", "Dave", "Erin", 
+            "Fiona", "George", "Hannah", "Ian", "Judy",
+            "Kevin", "Liam", "Mona", "Nina", "Oscar",
+            "Paula", "Quinn", "Rita", "Steve", "Tina",
+            "Uma", "Victor", "Wendy", "Xander", "Yara", "Zane"
+        ][:n_players]
+        shuffle(self.player_names)
+        
+        self.players = self.get_players()
+        self.game_engine = self.get_game_engine()
+
+    
+    def get_players(self):
+        return [
+            AIPlayer(
+                name=self.player_names[i], 
+                llm_model_name=self.impostor_model if i < self.n_impostors else self.crewmate_model, 
+                role="Impostor" if i < self.n_impostors else "Crewmate"
+            )
+            for i in range(self.n_players)
+        ]
+    
+
+    def get_game_engine(self):
+        game_engine = GameEngine(file_path=self.full_path)
+        game_engine.state.DEBUG = self.debug
+        game_engine.load_players(self.players, impostor_count=self.n_impostors)
+        game_engine.state.game_stage = GamePhase.ACTION_PHASE
+
+        return game_engine
+
+    
+    def run(self):
+        res = False
+        # while game is not finished and number of rounds doesn't exceed the limit
+        while not res and self.game_engine.state.round_number < self.n_round_cut_off:
+            try:
+                res = self.game_engine.perform_step()  # game is finished
+            except Exception as e:
+                if "LLM did" in str(e):
+                    continue
+                else:
+                    self.add_file_name_flag("exception")
+                    break
+
+        if self.game_engine.state.round_number >= self.n_round_cut_off:  # too many rounds
+            self.add_file_name_flag("round_limit")
+        
+
+    def add_file_name_flag(self, flag):
+        new_file = self.full_path.split('.')[0] + '_' + flag + '.json'
+        os.rename(self.full_path, new_file)
+
+
+class Tournament():
+    def __init__(self, ai_models, n_repetitions, n_players, n_impostors, dir, n_round_cut_off, debug):
+        self.ai_models = ai_models
+        self.n_repetitions = n_repetitions
+        self.n_ai_models = len(self.ai_models)
+        self.n_players = n_players
+        self.n_impostors = n_impostors
+        self.dir = dir
+        self.n_round_cut_off = n_round_cut_off
+        self.debug = debug
+    
+
+    def run(self):
+        for i, j in permutations(range(self.n_ai_models), 2):
+            for repetition in range(self.n_repetitions):
+                print(f"Starting new game!\nGame {self.ai_models[i].split('/')[-1].replace('.', '-')} vs {self.ai_models[j].split('/')[-1].replace('.', '-')} number {repetition + 1}\n")
+                game = TournamentGame(
+                    n_players=self.n_players,
+                    n_impostors=self.n_impostors,
+                    impostor_model=self.ai_models[i],
+                    crewmate_model=self.ai_models[j],
+                    dir=self.dir,
+                    file_path=self.ai_models[i].split("/")[-1].replace(".", "-") + 
+                        "_vs_" + 
+                        self.ai_models[j].split("/")[-1].replace(".", "-") + 
+                        "_" + 
+                        str(repetition + 1) +
+                        ".json",
+                    n_round_cut_off=self.n_round_cut_off,
+                    debug=self.debug
+                )
+                game.run()
+                print("\nGame finished!\n")
+        print("Tournament finished!")
+
+
+tournament = Tournament(
+    ai_models=ai_models,
+    n_repetitions=n_repetitions,
+    n_players=n_players,
+    n_impostors=n_impostors, 
+    dir=dir,
+    n_round_cut_off=n_round_cut_off,
+    debug=debug
+)
+
+tournament.run()
---
+++ src/llm_postor/tournament_analysis.py
@@ -0,0 +1,59 @@
+from itertools import permutations
+from llm_postor.game.game_engine import GameEngine
+import pandas as pd
+import os
+
+# Variables
+n_repetitions=1
+n_players = 5
+n_impostors = 1
+dir = 'data/tournament'
+n_round_cut_off = 3
+debug = True
+ai_models = [
+    "openai/gpt-4o-mini",
+    "google/gemini-flash-1.5",
+    "meta-llama/llama-3.1-8b-instruct"
+]
+
+OUTPUT = []
+
+file_names = list(os.walk(dir))[0][2]
+
+for i, j in permutations(range(len(ai_models)), 2):
+    wins, loses, round_limits, exceptions, game_prices = 0, 0, 0, 0, [0 for _ in range(n_repetitions)]
+    for repetition in range(n_repetitions):
+        file_path_pattern=(
+            ai_models[i].split("/")[-1].replace(".", "-") +
+            "_vs_" +
+            ai_models[j].split("/")[-1].replace(".", "-") +
+            "_" +
+            str(repetition + 1)
+        )
+        file_path = dir + "/" + next(filter(lambda x: file_path_pattern in x, file_names))
+
+        game_engine = GameEngine()
+        game_engine.load_state(file_path)
+
+        if "exception" in file_path:
+            exceptions += 1
+        elif "round_limit" in file_path:
+            round_limits += 1
+        else:
+            if game_engine.check_impostors_win():
+                wins += 1
+            else:
+                loses += 1
+
+        game_prices[repetition] = game_engine.state.get_total_cost()["total_cost"]
+    
+    OUTPUT.append([
+        ai_models[i].split('/')[-1].replace('.', '-'), ai_models[j].split('/')[-1].replace('.', '-'),
+        wins, loses, exceptions, round_limits, sum(game_prices) / len(game_prices)
+    ])
+    
+    print(f"{ai_models[i].split('/')[-1].replace('.', '-')} vs {ai_models[j].split('/')[-1].replace('.', '-')}")
+
+output_pd = pd.DataFrame(OUTPUT, columns=["Impostor", "Crewmate", "Wins", "Loses", "Exceptions", "Round limits", "Average game price"])
+output_pd.to_csv("tournament_analysis.csv")
+print(output_pd)
