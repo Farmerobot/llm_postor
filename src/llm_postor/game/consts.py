9a6aa056e0873b4d95fd6635f3b556053cda2b8a
Luncenok <mateusz.idziejczak@gmail.com>
2024-11-14 21:35:18 +0100

    add models

--- src/llm_postor/game/consts.py
+++ src/llm_postor/game/consts.py
@@ -4,74 +4,104 @@ NUM_CHATS = 5
 IMPOSTOR_COOLDOWN = 1
 STATE_FILE = "data/game_state.json"
 TOKEN_COSTS = {
-    "openai/gpt-4o": {
-		"input_tokens": 2.5,
-		"cache_read": 1.25,
-		"output_tokens": 10,
-	},
-    "openai/gpt-4o-mini": {
-		"input_tokens": 0.15,
-		"cache_read": 0.075,
-		"output_tokens": 0.6,
-	},
-	"google/gemini-flash-1.5": {
-		"input_tokens": 0.075,
+    # OPENAI
+    "openai/gpt-4o": {  # 128k context
+        "input_tokens": 2.5,
+        "cache_read": 1.25,
+        "output_tokens": 10,
+    },
+    "openai/gpt-4o-mini": {  # 128k context
+        "input_tokens": 0.15,
+        "cache_read": 0.075,
+        "output_tokens": 0.6,
+    },
+    
+    # CLAUDE
+    "anthropic/claude-3.5-sonnet": {  # 200k context
+        "input_tokens": 3,
 		"cache_read": 0,
-		"output_tokens": 0.3,
+		"output_tokens": 15,
 	},
-	"gryphe/mythomax-l2-13b": {
-		"input_tokens": 0.9,
+    "anthropic/claude-3.5-haiku": {  # 200k context
+        "input_tokens": 1,
 		"cache_read": 0,
-		"output_tokens": 0.9,
+		"output_tokens": 5,
 	},
-	"microsoft/wizardlm-2-8x22b": {
-		"input_tokens": 0.5,
-		"cache_read": 0,
-		"output_tokens": 0.5,
-	},
-	"microsoft/wizardlm-2-7b": {
-		"input_tokens": 0.055,
-		"cache_read": 0,
-		"output_tokens": 0.055,
-	},
-	"qwen/qwen-2-7b-instruct": {
-		"input_tokens": 0.054,
-		"cache_read": 0,
-		"output_tokens": 0.054,
-	},
-	"meta-llama/llama-3.1-8b-instruct": {
-		"input_tokens": 0.05,
-		"cache_read": 0,
-		"output_tokens": 0.05,
-	},
-	"meta-llama/llama-3.2-1b-instruct": {
-		"input_tokens": 0.01,
-		"cache_read": 0,
-		"output_tokens": 0.02,
-	},
-	"meta-llama/llama-3.2-3b-instruct": {
-		"input_tokens": 0.03,
-		"cache_read": 0,
-		"output_tokens": 0.05,
-	},
-    "meta-llama/llama-3.1-70b-instruct": {
-        "input_tokens": 0.35,
+    
+    # GOOGLE
+    "google/gemini-flash-1.5": {  # 1M context
+        "input_tokens": 0.075,
         "cache_read": 0,
-        "output_tokens": 0.4,
-	},
-	"nousresearch/hermes-3-llama-3.1-405b": {
-		"input_tokens": 1.79,
-		"cache_read": 0,
-		"output_tokens": 2.49,
-	},
-	"mistralai/mistral-nemo": {
-		"input_tokens": 0.13,
-		"cache_read": 0,
-		"output_tokens": 0.13,
-	},
-	"eva-unit-01/eva-qwen-2.5-32b": {
-		"input_tokens": 0.5,
-		"cache_read": 0,
-		"output_tokens": 0.5,
-  }
+        "output_tokens": 0.3,
+    },
+    "google/gemini-pro-1.5": {  # 2M context
+        "input_tokens": 1.25,
+        "cache_read": 0,
+        "output_tokens": 5,
+    },
+    
+    # META
+    "meta-llama/llama-3.2-1b-instruct": {  # 131k context
+        "input_tokens": 0.01,
+        "cache_read": 0,
+        "output_tokens": 0.02,
+    },
+    "meta-llama/llama-3.2-3b-instruct": {  # 131k context
+        "input_tokens": 0.03,
+        "cache_read": 0,
+        "output_tokens": 0.05,
+    },
+    "meta-llama/llama-3.1-8b-instruct": {  # 16k-131k context
+        "input_tokens": 0.05,
+        "cache_read": 0,
+        "output_tokens": 0.05,
+    },
+    "meta-llama/llama-3.1-70b-instruct": {  # 33k-131k context
+        "input_tokens": 0.34,
+        "cache_read": 0,
+        "output_tokens": 0.39,
+    },
+    "meta-llama/llama-3.1-405b-instruct": {  # 33k-131k context
+        "input_tokens": 1.79,
+        "cache_read": 0,
+        "output_tokens": 1.79,
+    },
+    
+    # MICROSOFT
+    "microsoft/wizardlm-2-7b": {  # 32k context
+        "input_tokens": 0.055,
+        "cache_read": 0,
+        "output_tokens": 0.055,
+    },
+    "microsoft/wizardlm-2-8x22b": {  # 66k context
+        "input_tokens": 0.5,
+        "cache_read": 0,
+        "output_tokens": 0.5,
+    },
+    
+    # ROLEPLAY 
+    # 1. mythomax (too small context) 4k
+    # 2. llama 8b 
+    # 3. llama 70b 
+    # 4. sonnet 3.5
+    # 5. mistral-nemo 
+    # 6. wizard 8x22b 
+    # 7. wizard 2-7b 
+    # 8. nous hermes 405b
+    # ?. qwen eva
+    "mistralai/mistral-nemo": { # 128k context
+        "input_tokens": 0.15,
+        "cache_read": 0,
+        "output_tokens": 0.15,
+    },
+    "nousresearch/hermes-3-llama-3.1-405b": {
+        "input_tokens": 1.79,
+        "cache_read": 0,
+        "output_tokens": 2.49,
+    },
+    "eva-unit-01/eva-qwen-2.5-32b": {  # 32k context
+        "input_tokens": 0.5,
+        "cache_read": 0,
+        "output_tokens": 0.5,
+    },
 }
